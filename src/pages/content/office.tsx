61469d67b1def37bea91656772ca09ef5b9d2394
Saenyakorn <saneyakorn@gmail.com>
2021-08-14 17:03:40 +0700

    add content page

--- src/pages/content/office.tsx
+++ src/pages/content/office.tsx
@@ -17,57 +17,73 @@ export default function StartPage() {
   const router = useRouter()
   const { index } = router.query
 
-  const normalContents: BlogBlockProps[] = [
-    {
-      title: 'Office Syndrome คืออะไร?',
-      image: '/content/1.jpg',
-    },
-    {
-      title: 'เคล็ดลับ การบรรเทา Office Syndrome',
-      image: '/content/2.jpg',
-    },
-    {
-      title: 'จะนั่งจะยืนยังไงให้ หลังชนะ?',
-      image: '/content/3.jpg',
-    },
-  ]
-
-  const specialContents: BlogBlockProps[] = [
-    {
-      title: 'เทรนเนอร์แบมแบมยอมรับเคยเป็น Office Syndrome แต่หายได้ด้วยวิธี.... ',
-      image: '/content-ex/1.png',
-    },
-    {
-      title: 'รู้หรือไม่ ? ไม่ต้องนั่งทำงานก็เป็น Office Syndrome ได้ หากคุณมีพฤติกรรมแบบนี้',
-      image: '/content-ex/2.png',
-    },
-    {
-      title: '4 วิธีจัดโต๊ะทำงาน ที่นั่งกี่ชั่วโมง ก็ไม่เมื่อย !',
-      image: '/content-ex/3.png',
-    },
-  ]
-
   return (
     <>
       <Container maxWidth="md">
         <Typography variant="h4" color="primary" sx={{ marginBottom: 2 }}>
           บทความ
         </Typography>
-        <Box mb={4}>
-          <Stack flexWrap="wrap" direction="row">
-            {normalContents.map((props, index) => (
-              <BlogBlock key={`nor-${index}`} {...props} />
-            ))}
-          </Stack>
-        </Box>
-        <Box mb={4}>
-          <Typography variant="h6">หัวข้อพิเศษ</Typography>
-          <Stack flexWrap="wrap" direction="row">
-            {specialContents.map((props, index) => (
-              <BlogBlock key={`spe-${index}`} {...props} />
-            ))}
-          </Stack>
-        </Box>
+        <Image src={MainImage} alt="icon" width="1248" height="511" loading="lazy" />
+        <Typography variant="h4" color="primary" sx={{ marginTop: 2, marginBottom: 2 }}>
+          Office Syndrome คืออะไร?
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          ออฟฟิตซินโดรม (Office Syndrome ) หรืออาการปวดกล้ามเนื้อและพังผือ เป็นอาการที่มีจากการใช้กล้ามเนื้อ หรือ
+          การทำสิ่งเดิมๆเป็นละยะเวลานานๆ ยกตัวอย่างเช่น การนั่งเล่นโทรศัพท์นานๆโดยก้มคอมอง
+          หรือการนั่งทำงานเป็นเวลานาเกินไป โดยไม่ได้ผ่อนคลาย หรือเปลี่ยนรูปแบบการนั่ง ทำให้มีอาการปวด
+          ร่วมไปถึงอาการชาร่วมด้วย ซึ่งอาการอาจจะรุกลามเป็นอาการปวเเรื้อรัง การดูแลอาการตั้งแต่เนิ่นๆ
+          จะลดความเสี่ยงในการเกิดของอาการหรือโรค ออฟฟิตซินโดรม
+        </Typography>
+
+        <Typography variant="h6" color="primary" sx={{ marginTop: 2, marginBottom: 2 }}>
+          Office Syndrome คืออะไร?
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          มีอาการปวดตามส่วนต่างๆเฉพาะส่วน เช่น ไหล่ หอ บ่า มีอาการปวดเป็นวงกว้าง หรือบริเวณใกล้เคียง
+          ความปวดของกล้ามเนื้อจะมีความปวดตั้งแต่เล็กน้อยจนไปถุงปวกระดับรุนแรงและทรมาน
+          อาการของระบบประสาทอัตโนมัติพบร่วมด้วย เช่น ซ่า วูบ เย็น ซีด ขนลุก และเหงื่ออก ตามจุดที่มีอาการปวดหรือชา
+          ถ้าเกิดบริเวณคออาจจจะมีอาการตาพร่า หูอื้อหรือมึนงง มีอาการชา เนื่องจากถูกกดทับ เช่น มือและแขน มีอาการอ่อนแรง
+          หรอกถูกกดทับนานเกินไป
+        </Typography>
+
+        <Typography variant="h6" color="primary" sx={{ marginTop: 2, marginBottom: 2 }}>
+          การรักษาและการบรรเทา
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          หากมีอาการ ออฟิตซินโดรม
+          ควรได้รับการรักษาโดยแพทย์เวชศาสตร์ฟื้นฟูที่มีความเชี่ยวชาญด้านการรํกษาอาการปวดกล้ามเนื้อ
+          โดยการรักษาจะทำการวินิจฉัยจากแพทย์ และหาทางเลือกในการรักษาที่เหมาะสมของแต่ละบุคคล
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          1.การรักษาด้วยทางกายภาพบำบัด ด้วยอุปกรณ์ที่ทันสมัย
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          2.การฝั่งเข็ม
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          3.การยืดกล้ามเนื้อที่ถูกวิธีด้วยตัวเอง
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          4.การนวดแผนไทย
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          5.การรับประทานยา
+        </Typography>
+
+        <Typography variant="h6" color="primary" sx={{ marginTop: 2, marginBottom: 2 }}>
+          การป้องกันการเกิดออฟฟิตซินโดม
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          1.การออกกำลังกายที่ถูกท่าถูกวิธี ที่เหมาะสมกับอาการ เช่น การออกกำลังกายเพื่อเสริมความเเข็งแรงของกลามเนื้อ
+          การออกกำลังการเพื่อเพิ่มความยืดหยุ่นให้กับกล้ามเนื้อ
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          2.การเปลี่ยนพฟติกรรมการใช้งานกล้ามเนื้อให้เหมาะสม เช่น ในระหว่างทำงานควรยืดก้ามเนื้อ ทุกๆ1 ชม.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          3.การปรับสภาพแวดล้อมในการทำงาน เช่นการเปลี่ยนโต๊ะหรือเก้าอี้ การปรับระดับความสูงของโต๊ะและเก้าอี้
+          ปรับหน้าจอให้อยู่ระดับสายตาต่ออผู้ใช้งาน
+        </Typography>
       </Container>
     </>
   )
